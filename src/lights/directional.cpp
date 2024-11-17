944849e3d36c978661949dee0c0e03ec037defe4
gaurarihant <arihant.gaur.usa@gmail.com>
2024-11-17 10:39:19 +0100

    feat:add point and directional light

---
+++ src/lights/directional.cpp
@@ -0,0 +1,37 @@
+#include <lightwave.hpp>
+
+namespace lightwave {
+
+class DirectionalLight final : public Light {
+
+    Color m_intensity;
+    Vector m_direction;
+
+public:
+    DirectionalLight(const Properties &properties) : Light(properties) {
+        // blender_exporter/lightwave_blender/light.py has two properties for directional
+        // source: direction and intensity
+        m_direction = properties.get<Vector>("direction");
+        m_intensity = properties.get<Color>("intensity");
+    }
+
+    DirectLightSample sampleDirect(const Point &origin,
+                                   Sampler &rng) const override {
+        // NOT_IMPLEMENTED  
+        // return format: direction, sample weight, distance from queried point to sampled point
+        return { .wi =  m_direction.normalized(), .weight = m_intensity, .distance = Infinity};
+        
+    }
+
+    bool canBeIntersected() const override { return false; }
+
+    std::string toString() const override {
+        return tfm::format(
+            "DirectionalLight[\n"
+            "]");
+    }
+};
+
+} // namespace lightwave
+
+REGISTER_LIGHT(DirectionalLight, "directional")
--- src/lights/point.cpp
+++ src/lights/point.cpp
@@ -4,17 +4,27 @@ namespace lightwave {
 
 class PointLight final : public Light {
 
-    ref<Light> point_light;
+    Color m_power;
+    Point m_position;
 
 public:
     PointLight(const Properties &properties) : Light(properties) {
-        point_light = properties.get<Light>("point");   
+        // blender_exporter/lightwave_blender/light.py has two properties for point source: position and power
+        m_position = properties.get<Point>("position");
+        m_power = properties.get<Color>("power", Color::black());
     }
 
     DirectLightSample sampleDirect(const Point &origin,
                                    Sampler &rng) const override {
         // NOT_IMPLEMENTED  
-        point_light->
+        // return format: direction, sample weight, distance from queried point to sampled point
+        Vector dir = m_position - origin;
+        float dist = dir.length();
+
+        // Convert power to intensity (Ref. Pg. 28 lecture slides)
+        Color intensity = (m_power * Inv4Pi) / sqr(dist);
+
+        return { .wi =  dir.normalized(), .weight = intensity, .distance = dist};
         
     }
 
