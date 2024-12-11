2a6cf5b6309941e3a6aefa59c00afae73fc61379
Kshitij Kumar <ksks00001@stud.uni-saarland.de>
2024-12-11 12:00:48 +0100

    feat:added pathtracer.cpp

---
+++ src/integrators/pathtracer.cpp
@@ -0,0 +1,85 @@
+#include <lightwave.hpp>
+
+namespace lightwave {
+
+/**
+ * @brief Codebase for direct lighting integrator
+ */
+
+class PathTracer : public SamplingIntegrator {
+
+public:
+    PathTracer(const Properties &properties)
+        : SamplingIntegrator(properties){
+    }
+
+    Color Li(const Ray &ray, Sampler &rng) override {
+        // Color weighted = Color(1.0f);
+        Ray primary_ray = Ray(ray.origin, ray.direction, ray.depth);
+        Color final_color = Color(0.0f);
+        // for (int depth = 0; depth < 2; depth++){
+            // (a) First scene intersection
+            Intersection its = m_scene->intersect(primary_ray, rng);
+
+            // (b) check for no intersection
+            if(!its){  
+                EmissionEval x = its.evaluateEmission();
+                return x.value;
+            }
+
+            // final_color += its.evaluateEmission().value;
+
+        // int depth_counter = ray.depth;
+
+        
+        
+            if(m_scene->hasLights()){
+                // (c) SampleLight function
+                LightSample light_sample = m_scene->sampleLight(rng);
+                if(!light_sample.isInvalid()){ // Removes segmentation fault
+                    DirectLightSample dls = light_sample.light->sampleDirect(its.position, rng); 
+                    // (d) Tracing secondary ray
+                    Ray shadow_ray = Ray(its.position, dls.wi);
+                    Color bsdf_eval = its.evaluateBsdf(dls.wi).value;
+
+                    bool occluded = m_scene->intersect(shadow_ray, dls.distance, rng);
+                    if(!occluded) final_color += (1 / light_sample.probability) * dls.weight * bsdf_eval;
+                }
+            }
+
+            if(its.evaluateEmission().value == Color(0.0f)){
+
+                BsdfSample sample_ = its.sampleBsdf(rng);
+                Ray next_ray = Ray(its.position, sample_.wi.normalized());
+                Intersection next_its = m_scene->intersect(next_ray, rng);
+                final_color += sample_.weight * next_its.evaluateEmission().value;
+                // primary_ray.origin = next_its.position;
+                // primary_ray.direction = sample_.wi.normalized();
+                // primary_ray.depth -= 1;
+
+                
+            }   
+            else
+                final_color += its.evaluateEmission().value;  
+        // }
+        return final_color;
+
+
+    }
+
+    /// @brief An optional textual representation of this class, which can be
+    /// useful for debugging.
+    std::string toString() const override {
+        return tfm::format("PathTracer[\n"
+                           "  sampler = %s,\n"
+                           "  image = %s,\n"
+                           "]",
+                           indent(m_sampler), indent(m_image));
+    }
+
+
+};
+
+}
+
+REGISTER_INTEGRATOR(PathTracer, "pathtracer")
